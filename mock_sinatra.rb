db0b17a364ae0fa8af8291a45258d105827669a9
adamzuckerberg <adam@adamzuckerberg.com>
2015-07-10 10:07:01 -0700

    Added paths, before filters, routes, procs, and included in a class

--- mock_sinatra.rb
+++ mock_sinatra.rb
@@ -1,9 +1,35 @@
 module MockSinatra
-	
+
 	def self.included(klass)
 		puts "MockSinatra has been included"
 	end
 
+	def run(path)
+		if paths.has_key?(path)
+			before_filters[path].call
+			paths[path].call
+		else 
+			raise StandardError.new("No route for #{path}")
+		end
+	end
+
+	def paths
+		@paths ||= {}
+	end
+
+	def before_filters
+		@before_filters ||= {}
+	end
+
+	def before(path, &block)
+		before_filters[path] = block
+	end
+
+	def get(path, &block)
+		puts "Defining a GET request for #{path}"	
+		paths[path] = block
+		block.call
+	end
 
 end	
 
--- runner.rb
+++ runner.rb
@@ -1,3 +1,23 @@
 require "./mock_sinatra.rb"
 
-include MockSinatra
+class MyWebApp
+	include MockSinatra
+end
+
+app = MyWebApp.new
+
+app.get "/hi" do
+	puts "I am the /hi method"
+	puts "Hello, world!"
+end
+
+app.get "/hello" do
+	puts "I am the /hello method"
+end
+
+app.before "/hi" do
+	puts "I am running before /hi"
+	puts "-----------------------"
+end
+
+app.run("/hi")
